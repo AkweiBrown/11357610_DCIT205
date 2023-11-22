704daba5f65b287f8d82ae6d6fe7a8860de943e6
Ryan Brown <niiakweibrown1@gmail.com>
2023-11-22 11:21:00 -0800

    added introductory section

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-const images = [
-    'about-banner.jpg',
-    'img/hm-bg.jpg',
-    'img/hm-slideshow1.jpg',
-    'img/hm-bg1.jpg'
-    // Add more image URLs as needed
-  ];
+// const images = [
+//     'img/hm-bg1.jpg'
+//     // Add more image URLs as needed
+//   ];
   
-  let currentIndex = 0;
-  const introSection = document.querySelector('.intro-hm');
+//   let currentIndex = 0;
+//   const introSection = document.querySelector('.intro-hm');
   
-  function changeBackground() {
-    introSection.style.backgroundImage = `url(${images[currentIndex]})`;
-    introSection.classList.add('dark-overlay'); // Adding a CSS class for the overlay
-    currentIndex = (currentIndex + 1) % images.length;
-  }
+//   function changeBackground() {
+//     introSection.style.backgroundImage = `url(${images[currentIndex]})`;
+//     introSection.classList.add('dark-overlay'); // Adding a CSS class for the overlay
+//     currentIndex = (currentIndex + 1) % images.length;
+//   }
   
-  // Change background every 5 seconds (adjust as needed)
-  setInterval(changeBackground, 5000);
+//   // Change background every 5 seconds (adjust as needed)
+//   setInterval(changeBackground, 5000);
   
