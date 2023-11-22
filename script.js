4b4f889d4c2e85911d1d8918e217251b20cf093a
Ryan Brown <niiakweibrown1@gmail.com>
2023-11-22 13:35:13 -0800

    Simplified about page and footer added to homepage

--- script.js
+++ script.js
@@ -15,3 +15,29 @@
 //   // Change background every 5 seconds (adjust as needed)
 //   setInterval(changeBackground, 5000);
   
+
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to handle scroll event
+function handleScroll() {
+  const whatWeOfferSection = document.querySelector('.what-we-offer');
+  if (isInViewport(whatWeOfferSection)) {
+      whatWeOfferSection.classList.add('show');
+      window.removeEventListener('scroll', handleScroll);
+  }
+}
+
+// Add event listener for scroll
+window.addEventListener('scroll', handleScroll);
+
+// Initial check in case the element is already visible on page load
+handleScroll();
