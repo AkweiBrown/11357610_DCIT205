1fb7fef6cf8c58484a57efe9466c6663dfa16d73
Ryan Brown <niiakweibrown1@gmail.com>
2023-11-28 11:58:03 -0800

    Responsive homepage

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-// const images = [
-//     'img/hm-bg1.jpg'
-//     // Add more image URLs as needed
-//   ];
-  
-//   let currentIndex = 0;
-//   const introSection = document.querySelector('.intro-hm');
-  
-//   function changeBackground() {
-//     introSection.style.backgroundImage = `url(${images[currentIndex]})`;
-//     introSection.classList.add('dark-overlay'); // Adding a CSS class for the overlay
-//     currentIndex = (currentIndex + 1) % images.length;
-//   }
-  
-//   // Change background every 5 seconds (adjust as needed)
-//   setInterval(changeBackground, 5000);
-  
+
 
 // Function to check if an element is in the viewport
 function isInViewport(element) {
